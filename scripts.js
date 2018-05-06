4f599c991be3c0ad4bbc2bf0ee68a536c018a671
Jackson <jaesthetyc@gmail.com>
2018-05-06 16:16:58 -0500

    V 0.5

--- scripts.js
+++ scripts.js
@@ -2,13 +2,3 @@ function cwm() {
   document.getElementById('cwm').innerHTML = 'You cannot resist.';
   setTimeout(function(){document.getElementById('cwm').style.display = 'none';}, 1000)
 }
-function clicked() {
-  alert("Thank you for freeing me!");
-  document.getElementById('hover').style.display = 'none';
-}
-function over() {
-  document.getElementById('hover').innerHTML = "Please :(";
-}
-function out() {
-  document.getElementById('hover').innerHTML = "Click me!";
-}
