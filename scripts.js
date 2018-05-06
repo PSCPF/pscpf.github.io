3620b0d9c61e554ffd1917421f35fe22fe654750
Jackson <jaesthetyc@gmail.com>
2018-05-06 17:10:19 -0500

    v0.75

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 function cwm() {
-  document.getElementById('cwm').innerHTML = 'You cannot resist.';
-  setTimeout(function(){document.getElementById('cwm').style.display = 'none';}, 1000)
+  setTimeout(function(){document.getElementById('cwm').innerHTML = 'You cannot resist.';}, 4000)
+  setTimeout(function(){document.getElementById('cwm').style.display = 'none';}, 4500)
+}
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
 }
